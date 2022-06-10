fab8c6b7e863e6f2b341c79cb3745cb413866347
ericseti41 <eric.seti41@gmail.com>
2022-06-10 15:11:55 +0700

    update promise.js using async & await

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaction) => {
+  const ixx = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+
+  const ixxFiltered = ixx.filter((theater) => theater.hasil === reaction);
+  const vgcFiltered = vgc.filter((theater) => theater.hasil === reaction);
+
+  return ixxFiltered.length + vgcFiltered.length;
+};
 
 module.exports = {
   promiseOutput,
